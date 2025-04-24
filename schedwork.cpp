0f09d286faaa357d292e9fa890962cf511487b5e
Sumin Wu <suminwu@usc.edu>
2025-04-24 22:15:13 +0000

    final

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,22 +21,74 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool helper2(const AvailabilityMatrix& aMtrx, const Worker_T& hire, const size_t& maximumSlot, const DailySchedule& daily){
+    int slot=0;
+    for (int j=0;j<aMtrx.size();j++){
+        if(find(daily[j].begin(),daily[j].end(),hire)!=daily[j].end()){
+            slot++;
+        }
+    }
+    if(slot>maximumSlot){
+        return true;
+    }
+    return false;
+}
 
 
+bool helper(const AvailabilityMatrix& aMtrx, 
+            const size_t& dailyNeeds, 
+            size_t maxShifts, 
+            int dayz, 
+            DailySchedule& daily){
+    if(aMtrx.size()==dayz){
+        return true;//filled!
+    }
+    for(int j = 0;j<aMtrx[0].size();++j){
+        if(aMtrx[dayz][j]&&find(daily[dayz].begin(), daily[dayz].end(), j) ==daily[dayz].end()){
+                //avail but not schedueld for td
+            daily[dayz].push_back(j);
+        
+            if(helper2(aMtrx,j,maxShifts,daily)==false){
+                if(dailyNeeds==daily[dayz].size()){
+                    if(helper(aMtrx,dailyNeeds,maxShifts,dayz+1,daily)==true){
+                        return true;
+                    }
+                    
+                    
+                }
+                else if (dailyNeeds!=daily[dayz].size()==true){
+                        if(helper(aMtrx,dailyNeeds,maxShifts,dayz,daily)==true)
+                        {
+                            return true;
+                        }
+                    }
+                
+            }
+            daily[dayz].pop_back();
+        
+    }
+    
+}
+return false;
+}
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
-    const AvailabilityMatrix& avail,
+    const AvailabilityMatrix& aMtrx,//.size() is the number of dayz
     const size_t dailyNeed,
     const size_t maxShifts,
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(aMtrx.size() == 0U){
         return false;
     }
     sched.clear();
     // Add your code below
+    for (int j = 0; j<aMtrx.size(); j++){
+        sched.push_back(vector<Worker_T>());
+    }
+    return helper(aMtrx, dailyNeed, maxShifts,0,sched);
 
 
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+bool acceptOrReject(const std::string& in, std::string floats, int idx, std::string entry);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,48 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> final;
+    for(set<string>::iterator it = dict.begin(); it!=dict.end(); ++it){
+        bool temp = false;
+        if (in.size()!=  (*it).size() ){
+            continue;
+        }
+        for(char x = 'A'; x<='Z'; ++x){
+            if((*it).find(x)!=string::npos){
+                temp=true;
+                break;
+            }
+        }
+        if(temp){
+            continue;
+        }
+        if(acceptOrReject(in,floating,0,*it)){
+            final.insert(*it);
+        }
+    }
+    for(std::set<std::string>::iterator it = final.begin(); it!=final.end(); ++it){
+        std::cout<<*it<<" ";
+    }
+    std::cout<< endl;
+    return final;
 }
 
 // Define any helper functions here
+bool acceptOrReject(const std::string& in, std::string floats, int idx, std::string entry){
+    if(idx>=(int)entry.size() && floats==""){
+        return true;
+      
+    }
+    else if (idx>=(int)entry.size() && floats!=""){
+        return false;
+      }
+    else{
+        if(in[idx]!='-' && in[idx]!=entry[idx]){
+            return false;
+        }
+        else if(floats.find(entry[idx])!=string::npos){
+            floats.erase(floats.find(entry[idx]),1);
+        }
+    }
+    return acceptOrReject(in,floats,idx+1,entry);
+}
